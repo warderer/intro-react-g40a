1da2752c058065781ae56d863096ddcb4552d585
César Guerra <warderer@gmail.com>
2025-06-02 20:47:27 -0600

    ✨ react props

--- 01-react-props/src/main.jsx
+++ 01-react-props/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App saludo="Hola" title="G40A, Intro a React, soy un prop" />
+    <App saludo="Hi" title="This is another component with diferent props" />
   </StrictMode>,
 )
