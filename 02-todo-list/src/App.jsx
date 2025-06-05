8f196fec3efd50097fd8acdcdea1ab9bb3d3f129
César Guerra <warderer@gmail.com>
2025-06-04 20:57:21 -0600

    ✨ Finished ToDo App

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import ToDoItem from './components/ToDoItem'
 
 function App () {
   /* 1. Creo el estado donde guardare la información del input */
   const [inputValue, setInputValue] = useState('')
 
+  /* 4. Creo un nuevo estado para manejar la lista de tareas */
+  const [tasks, setTasks] = useState([])
+
   /* 3. Creo una función que se ejecuta cuando se hace clic a agregar */
+  /* 5. Modificar la función para agregar tareas a la lista */
   const handleAdd = () => {
-    console.log('Añadir tarea:', inputValue)
+    // console.log('Añadir tarea:', inputValue)
+    setTasks([...tasks, inputValue])
+    setInputValue('') // Limpiar el input después de añadir la tarea
   }
+
+  /* 6. Crear una función para eliminar tareas */
+  const deleteTodo = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index)
+    setTasks(newTasks)
+  }
+
   return (
     <>
       <h1>Todo List</h1>
@@ -22,9 +36,13 @@ function App () {
       <button onClick={handleAdd}>Añadir</button>
       {/* ul>li{Item $}*3>button{Eliminar} */}
       <ul>
-        <li>Item 1<button>Eliminar</button></li>
-        <li>Item 2<button>Eliminar</button></li>
-        <li>Item 3<button>Eliminar</button></li>
+        {tasks.map((task, index) => (
+          <ToDoItem
+            key={index}
+            todoName={task}
+            handleDelete={() => deleteTodo(index)}
+          />
+        ))}
       </ul>
     </>
   )
