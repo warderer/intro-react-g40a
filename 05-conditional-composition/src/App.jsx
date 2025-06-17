17a8b3ff492ac43cc9af17e5ff21bea7f1528027
César Guerra <warderer@gmail.com>
2025-06-16 19:52:17 -0600

    ✨ Renderizado condicional basico

--- 05-conditional-composition/src/App.jsx
+++ 05-conditional-composition/src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+function App () {
+  // Estado para saber si el usuario ha iniciado sesión
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // Funciones para simular el inicio y cierre de sesión
+  const handleLogin = () => {
+    setIsLoggedIn(true)
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+  }
 
   return (
-    <>
-      <div className='container'>
-        <h1>Mi Panel de Control</h1>
-      </div>
-    </>
+    <div className='container'>
+      <h1>Mi Panel de Control</h1>
+
+      {/* --- RENDERIZADO CONDICIONAL CON OP. TERNARIO */}
+
+      {
+        isLoggedIn
+          ? (
+            <div>
+              <button onClick={handleLogout}>Cerrar sesión</button>
+            </div>
+            )
+          : (
+            <div>
+              <button onClick={handleLogin}>Iniciar sesión</button>
+            </div>
+            )
+      }
+    </div>
   )
 }
 
