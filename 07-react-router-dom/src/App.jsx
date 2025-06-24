dde6d7550b5d04543fbcb1ee9045e81817e5662d
César Guerra <warderer@gmail.com>
2025-06-23 19:47:02 -0600

    ✨ Add Routing to project

--- 07-react-router-dom/src/App.jsx
+++ 07-react-router-dom/src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import AppRoutes from './routes/AppRoutes'
 import './App.css'
 
 function App () {
   return (
-    <>
-
-    </>
+    <div className='container'>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
   )
 }
 
