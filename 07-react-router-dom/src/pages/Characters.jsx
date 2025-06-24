a6aa422f87b39a0d94463dfd84006d0a6990384c
César Guerra <warderer@gmail.com>
2025-06-23 19:59:17 -0600

    ✨ Update Characters working with API

--- 07-react-router-dom/src/pages/Characters.jsx
+++ 07-react-router-dom/src/pages/Characters.jsx
@@ -1,6 +1,42 @@
+import { useState, useEffect } from 'react'
+// TODO: Importar Link de react-router-dom
+
 const Characters = () => {
+  const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        setCharacters(data.results)
+      } catch (error) {
+        console.error('Error al obtener los personajes:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCharacters()
+  }, [])
+
+  if (loading) return <p>Cargando personajes...</p>
+
   return (
-    <h1>Characters</h1>
+    <div>
+      <h1>Personajes de Rick and Morty</h1>
+      <div className='characters-grid'>
+        {characters.map((character) => (
+          // TODO: Implementar Link
+          <a href={`/character/${character.id}`} key={character.id} className='character-card'>
+            <img src={character.image} alt={character.name} />
+            <h3>{character.name}</h3>
+          </a>
+        ))}
+      </div>
+    </div>
   )
 }
+
 export default Characters
