27294f6d81bce1aefe00649f909275c542605090
César Guerra <warderer@gmail.com>
2025-06-25 19:40:41 -0600

    ✨ Add auth logic to App

--- 07-react-router-dom/src/App.jsx
+++ 07-react-router-dom/src/App.jsx
@@ -1,14 +1,33 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import AppRoutes from './routes/AppRoutes'
 import './App.css'
 
 function App () {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    // Inicializar el estado desde localStorage
+    return localStorage.getItem('isAuth') === 'true'
+  })
+
+  // Actualizar localStorage cuando el estado cambie
+  useEffect(() => {
+    localStorage.setItem('isAuth', isLoggedIn)
+  }, [isLoggedIn])
+
+  const login = () => {
+    setIsLoggedIn(true)
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false)
+  }
+
   return (
     <div className='container'>
       <BrowserRouter>
-        <Navbar />
-        <AppRoutes />
+        <Navbar isAuth={isLoggedIn} onLogin={login} onLogout={logout} />
+        <AppRoutes isAuth={isLoggedIn} />
       </BrowserRouter>
     </div>
   )
